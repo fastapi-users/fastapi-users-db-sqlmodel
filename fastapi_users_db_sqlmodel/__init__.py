94c9090a109cb93fd48950df4f451ec058572229
François Voron <fvoron@gmail.com>
2022-04-28 09:48:17 +0200

    Bump version: 0.1.1 → 0.1.2

--- fastapi_users_db_sqlmodel/__init__.py
+++ fastapi_users_db_sqlmodel/__init__.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 from sqlmodel import Field, Session, SQLModel, func, select
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 class SQLModelBaseUserDB(BaseUserDB, SQLModel):
